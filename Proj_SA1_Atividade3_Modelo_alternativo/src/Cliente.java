08da03af66802ba7014a4d4d1bdb0a705bc0fbe1
rolfiluz <89606223+rolfiluz@users.noreply.github.com>
2021-11-24 11:48:07 -0300

    Add files via upload

---
+++ Proj_SA1_Atividade3_Modelo_alternativo/src/Cliente.java
@@ -0,0 +1,30 @@
+
+public  class Cliente {
+
+	// Trocou-se Integer por int
+	public int agencia;
+
+	public int conta;
+
+	public String cpf;
+
+	public String nome ;
+
+	public String nascimento;
+
+	public String endereco ;
+
+	public String telefone;
+	
+	//Trocou-se Float float
+	public float saldo ;
+
+	public String senha ;
+
+	public  void MostrarCliente() {
+		System.out.println("Agência:"+ agencia);
+	
+
+	}
+
+}
---
+++ Proj_SA1_Atividade3_Modelo_alternativo/src/Execucao.java
@@ -0,0 +1,21 @@
+
+public class Execucao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Cliente cl1=new Cliente();
+		cl1.agencia = 100;
+		cl1.conta = 34567;
+		cl1.nome = "José da Silva";
+		cl1.nome="Rolfi";
+		
+		System.out.println("Agência:" + cl1.agencia);
+		System.out.println("Conta:" +cl1.conta);
+	    System.out.println("Nome:"+cl1.nome);
+	    
+	    
+
+	}
+
+}
